5d4099aed80a77dba1719278388b714e3ec52273
jooher <jooher@gmail.com>
2025-02-03 18:14:10 +0300

    works

--- AccelFlow/maths.js
+++ AccelFlow/maths.js
@@ -1,114 +1,91 @@
 "use strict";
 
-let	cx=0, // 
-	cy=0, // absolute y
-	cxn=0, // past normalized x,y and length
-	cyn=1,
-	absx=0,
-	absy=0,
-	;
-	
 const
 
 round = n => n, // Math.round, // n => .1 * Math.round(n*10),//
 
-len = (x,y) => Math.sqrt(x*x+y*y),
+parse = d => {
+	
+	const shape = d.split(" ")
+		.filter(str=>str.match(/^[0-9.,\-]+$/))
+		.map(str=>str.split(","))
+		.map( ([x,y])=>[1.0*x,1.0*y] );
+	
+	if("Z")
+		shape.push(shape[0]);
+	
+	return shape;
+};
 
 
-offset = (R,side) => ([x,y]) =>{
-	const
-		n = 1/len(x,y),
-		xn = x*n,
-		yn = y*n,
-		
-		ctg = (cyn-yn)/(cxn+xn),
-		
-		abs = Math.abs(ctg), //
-		sgn = Math.sign(ctg),
-		
-		r = sgn == side ? R : round(R/abs),
-		
-		rx = + r*(sgn*cyn-abs*cxn), // relative
-		ry = - r*(abs*cyn+sgn*cxn),
-		
-		// trim
-		s = Math.sqrt(rx*rx + ry*ry - r*r),
-		
-		// cx & cy here from previous step
-		lx = round(cx - s*cxn),
-		ly = round(cy - s*cyn),
-		
-		ax = round(s*(xn+cxn)),
-		ay = round(s*(yn+cyn)),
-		
-		color = ctg > 0 ? "green" : "red";
-		
-	absx+=x;
-	absy+=y;
-		
-	cx = x-s*xn;
-	cy = y-s*yn;
-	cxn = xn;
-	cyn = yn;
-/*	
-	return [
-		["circle",{title:`ctg=${ctg}`,cx,cy,r,style:`fill:${color};stroke:none`}],
-		["line",{x1,y1,x2,y2,stroke:"black"}]
-	];
-*/
+let absx,absy,cxn,cyn,cx,cy;
+	
+const
 
-	return [
-		"l",lx,ly,
-		"a",r,r,0,0,sgn>0?0:1,ax,ay
-		].join(" "); // a rx ry x-axis-rotation large-arc-flag sweep-flag dx dy
-	//
+norm = ([X,Y]) => {
+	const 
+		x = X - absx,
+		y = Y - absy,
+		n = 1/Math.sqrt(x*x+y*y);
+		
+	return [x*n,y*n];
 },
 
-rounds = (R,side) => ([x,y]) => {
+start = corners => {
+	[absx,absy] = corners.at(-2);
+	[cxn,cyn] = norm( corners.at(-1) );
+	[cx,cy] = corners.at(-1);	
+	return corners;
+},
+
+offset = T => // tool radius
+corners => start(corners).map( corner => {
 	const
-		n = 1/len(x,y),
-		xn = x*n,
-		yn = y*n,
+		[xn,yn] = norm(corner),
+		ctg = (cyn-yn)/(cxn+xn),
+		hx = -T*(cyn-ctg*cxn),
+		hy = +T*(cxn+ctg*cyn),
+		point = [ round(absx+hx), round(absy+hy) ];
+		
+	[absx,absy] = corner;
+	cxn = xn;
+	cyn = yn;
+			
+	return point;
+}),
+
+smooth = (R,A) => // tool radius, allowance
+corners => start(corners).map( corner => { 
+	const
+		[xn,yn] = norm(corner),
 		
 		ctg = (cyn-yn)/(cxn+xn),
 		
 		abs = Math.abs(ctg), //
 		sgn = Math.sign(ctg),
 		
-		r = sgn == side ? R : round(R/abs),
-		
-		rx = + r*(sgn*cyn-abs*cxn), // relative
-		ry = - r*(abs*cyn+sgn*cxn),
+		r = (sgn*R)>0 ? R : round(A/abs),
+		rx = - r*(sgn*cyn-abs*cxn),
+		ry = + r*(abs*cyn+sgn*cxn),
 		
 		// trim
 		s = Math.sqrt(rx*rx + ry*ry - r*r),
 		
 		// cx & cy here from previous step
-		lx = round(cx - s*cxn),
-		ly = round(cy - s*cyn),
+		lx = round(absx - s*cxn),
+		ly = round(absy - s*cyn),
 		
 		ax = round(s*(xn+cxn)),
 		ay = round(s*(yn+cyn)),
 		
 		color = ctg > 0 ? "green" : "red";
 		
-	absx+=x;
-	absy+=y;
-		
-	cx = x-s*xn;
-	cy = y-s*yn;
+	[absx,absy] = corner;		
 	cxn = xn;
 	cyn = yn;
-/*	
-	return [
-		["circle",{title:`ctg=${ctg}`,cx,cy,r,style:`fill:${color};stroke:none`}],
-		["line",{x1,y1,x2,y2,stroke:"black"}]
-	];
-*/
 
 	return [
-		"l",lx,ly,
-		"a",r,r,0,0,sgn>0?0:1,ax,ay
-		].join(" "); // a rx ry x-axis-rotation large-arc-flag sweep-flag dx dy
-	//
-}
+		"L",lx,ly,
+		"a",r,r,0,0,sgn>0?0:1,ax,ay	// a rx ry x-axis-rotation large-arc-flag sweep-flag dx dy
+	].join(" "); 
+});
